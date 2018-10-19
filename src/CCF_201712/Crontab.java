6ef3c80e005175ae76113c4d1c1a9c28692ae96b
1mrliu <ld815626@hotmail.com>
2018-10-19 16:49:07 +0800

    Add 20171203 Crontab

---
+++ src/CCF_201712/Crontab.java
@@ -0,0 +1,12 @@
+package CCF_201712;
+
+/**
+ * Created by liudong on 2018/10/19.
+ * 20171203 Crontab
+ * 题目描述 http://118.190.20.162/view.page?gpid=T66
+ */
+public class Crontab {
+    public static void main(String[] args){
+
+    }
+}
