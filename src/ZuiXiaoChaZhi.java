a07e7cadfdbbaee3a72d2996034e5a5863066352
1mrliu <ld815626@hotmail.com>
2018-10-19 15:04:29 +0800

    AC 201712-1 最小差值

---
+++ src/ZuiXiaoChaZhi.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Created by liudong on 2018/10/19.
+ * CCF  201712-1
+ * 最小差值
+ */
+public class ZuiXiaoChaZhi {
+    public static void main(String[] args){
+        Scanner  sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] m = new int[n];
+        for(int i =0; i< n;i++){
+            m[i] = sc.nextInt();
+        }
+        int min = 1000;
+        for(int i = 0; i<m.length;i++){
+            for (int j = i+1; j < m.length; j++) {
+                if (Math.abs(m[i] - m[j]) < min) {
+                    min = Math.abs(m[i] - m[j]);
+                }
+            }
+        }
+        System.out.print(min);
+
+    }
+}
